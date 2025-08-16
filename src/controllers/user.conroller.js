1498a4b088c0d2f7c7e7d57ec4be3cf6e30d263b
PushpamSingh <pushpamsingh204@gmail.com>
2025-08-16 16:59:12 +0530

    reviewing and updating user Controllers and addded some routes file

--- src/controllers/user.conroller.js
+++ src/controllers/user.conroller.js
@@ -2,18 +2,19 @@ import { Asynchandler } from "../utils/Asynchandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
+import { isValidObjectId } from "mongoose";
 
-const generateAccessAndRefreshToken=async function(userId) {
+const generateAccessAndRefreshToken = async function (userId) {
     try {
-        const user=await User.findById(userId);
-        if(!user){
-            throw new ApiError(404,"Unauthorized ! user not found")
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new ApiError(404, "Unauthorized ! user not found")
         }
-        const accessToken=await user.generateAccessToken();
-        const refreshToken=await user.generateRefreshToken();
-        user.refreshtoken=refreshToken;
-        user.save({validateBeforeSave:false})
-        return {accessToken,refreshToken}
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+        user.refreshtoken = refreshToken;
+        user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
     } catch (error) {
         throw error;
     }
@@ -21,54 +22,54 @@ const generateAccessAndRefreshToken=async function(userId) {
 const Registeruser = Asynchandler(async (req, res) => {
     try {
         const { fullName, email, phone, role, password } = req.body
-        if([fullName,email,phone,role,password].some((data)=>data==="")){
-            throw new ApiError(404,"Fields are empty")
+        if ([fullName, email, phone, role, password].some((data) => data === "")) {
+            throw new ApiError(404, "Fields are empty")
         }
         const avatar = req.file;
-        if(!avatar){
-            throw new ApiError(404,"Image not found")
+        if (!avatar) {
+            throw new ApiError(404, "Image not found")
         }
         //Existing user
-        const existedUser=await User.findOne({
-            $or:[
-                {email:email},
-                {phone:phone}
+        const existedUser = await User.findOne({
+            $or: [
+                { email: email },
+                { phone: phone }
             ]
         })
-        if(existedUser){
-            throw new ApiError(409,"user already exist")
+        if (existedUser) {
+            throw new ApiError(409, "user already exist")
         }
-        if(password.length<6){
-            throw new ApiError(400,"password must be at least 6 characters")
+        if (password.length < 6) {
+            throw new ApiError(400, "password must be at least 6 characters")
         }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)){
-            throw new ApiError(400,"Please enter valid emailID")
+        if (!emailRegex.test(email)) {
+            throw new ApiError(400, "Please enter valid emailID")
         }
-        const uploadOnCLoudinary=uploadCloudinary(avatar)
-        if(!uploadOnCLoudinary){
-            throw new ApiError(400,"Cloudinary error ! image not uploaded")
+        const uploadOnCLoudinary = uploadCloudinary(avatar)
+        if (!uploadOnCLoudinary) {
+            throw new ApiError(400, "Cloudinary error ! image not uploaded")
         }
 
-        const user=await User.create({
+        const user = await User.create({
             fullName,
             email,
             phone,
-            avatar:uploadOnCLoudinary?.url,
+            avatar: uploadOnCLoudinary?.url,
             role,
             password
         })
-        await user.save({validateBeforeSave:false})
+        await user.save({ validateBeforeSave: false })
 
-        if(!user){
-            throw new ApiError(400,"User not created")
+        if (!user) {
+            throw new ApiError(400, "User not created")
         }
-        const CreatedUser=await User.findById(user?._id)
-        if(!CreatedUser){
-            throw new ApiError(404,"user not found")
+        const CreatedUser = await User.findById(user?._id)
+        if (!CreatedUser) {
+            throw new ApiError(404, "user not found")
         }
         return res.status(200).json(
-            new ApiResponse(200,CreatedUser,"user created successfully")
+            new ApiResponse(200, CreatedUser, "user created successfully")
         )
     } catch (error) {
         res.status(500).json(
@@ -81,28 +82,51 @@ const Loginuser = Asynchandler(async (req, res) => {
     try {
         const { email, phone, password } = req.body;
         // Check if data is available
-        if([email, phone, password].some((data)=>data ==="")){
-            throw new ApiError(404, "Please give some input")
+        if ([email, phone, password].some((data) => data === "")) {
+            throw new ApiError(404, "email or password cannot be empty")
         }
 
-        if(password.length<8){
-            throw new ApiError(400, "Password must be at least 8 characters");
-        }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if(!emailRegex.test(email)){
-            throw new ApiError(400, "Please enter valid emailID ");
+        if (!emailRegex.test(email)) {
+            throw new ApiError(400, "Please enter valid emailID");
         }
 
-       const user = await User.findOne({
-        $or:[{email, phone}]
-       })
+        const user = await User.findOne({
+            $or: [{ email, phone }]
+        })
 
-       if(!user){
-        throw new ApiError(404, "User not Found");
-       }
+        if (!user) {
+            throw new ApiError(401, "Invalid phone number or email");
+        }
 
-        
+        if (password.length < 6) {
+            throw new ApiError(400, "Password must be at least 6 characters");
+        }
+
+        const isPasswordMatch = await user.ComparePassword(password);
+        if (!isPasswordMatch) {
+            throw new ApiError(401, "Incorrect Password!")
+        }
+
+        const { accessToken, refreshToken } = generateAccessAndRefreshToken(user?._id)
+        const options = {
+            httpOnly: true,
+            secure: true,
+            samesite: "None",
+            path: '/'
+        }
+        res.status(200)
+            .cookie("accessToken", accessToken, options)
+            .cookie("refreshToken", refreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    { user, accessToken, refreshToken },
+                    "User Logged in successfuly"
+                )
+            )
+
     } catch (error) {
         res.status(500).json(
             new ApiError(500, error?.message)
@@ -113,10 +137,36 @@ const Loginuser = Asynchandler(async (req, res) => {
 const Logoutuser = Asynchandler(async (req, res) => {
     try {
         const userId = req.user?._id;
-        const deleteData = await User.findByIdAndUpdate(userId,{$unset:{refreshToken:1}});
-        return res.status(200).json(
-            new ApiResponse(200,deleteData,"Logout Successfully")
-        )
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Unauthorized ! Invalid userId")
+        }
+
+        const LoggedOutUser = await User.findByIdAndUpdate(
+            userId,
+            {
+                $set: {
+                    refreshToken: null
+                }
+            },
+            {
+                new: true
+            }
+        );
+        if (!LoggedOutUser) {
+            throw new ApiError(403, "Logout Failled")
+        }
+        const options = {
+            httpOnly: true,
+            secure: true,
+            samesite: "None",
+            path: '/'
+        }
+        return res.status(200)
+            .cookie("accessToken", options)
+            .cookie("refreshToken", options)
+            .json(
+                new ApiResponse(200, LoggedOutUser, "Logout Successfully")
+            )
     } catch (error) {
         res.status(500).json(
             new ApiError(500, error?.message)
@@ -127,9 +177,15 @@ const Logoutuser = Asynchandler(async (req, res) => {
 const GetCurrentuser = Asynchandler(async (req, res) => {
     try {
         const userId = req.user?._id;
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Unauthorized ! Invalid userId")
+        }
         const CurrentUser = await User.findById(userId);
+        if (!CurrentUser) {
+            throw new ApiError(404, "User not exist")
+        }
         return res.status(200).json(
-           new ApiResponse(200, CurrentUser, "Found Current User" )
+            new ApiResponse(200, CurrentUser, "Found Current User")
         )
     } catch (error) {
         res.status(500).json(
@@ -142,18 +198,42 @@ const ChangePassword = Asynchandler(async (req, res) => {
     try {
         const { oldPassword, newPassord } = req.body
         const userId = req.user?._id;
+        if (!(oldPassword || newPassord)) {
+            throw new ApiError(400, "input fields must not be empty")
+        }
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Unauthorized ! Invalid userId")
+        }
+        const user = await User.findById(userId)
+        if (!user) {
+            throw new ApiError(404, "user not found")
+        }
 
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        if(!passwordRegex.test(password)){
-            throw new ApiError (400, "The password must contain at least 8 characters One special characters and one Alphabet letter");
+        const isPasswordMatch = await user.ComparePassword(oldPassword)
+        if (!isPasswordMatch) {
+            throw new ApiError(403, "Unauthorize ! you entered wrong oldPassword")
         }
-        if(oldPassword===newPassord){
-            throw new ApiError(404, "Sorry you've entered the same password");
+
+        if (password.length < 6) {
+            throw new ApiError(400, "Password must be at least 6 characters");
         }
-        updatedPassword = await User.findByIdAndUpdate(userId);
+        const updatedPassword = await User.findByIdAndUpdate(
+            userId,
+            {
+                $set: {
+                    password: newPassord
+                }
+            },
+            {
+                new: true
+            }
+        );
+        if (!updatedPassword) {
+            throw new ApiError(400, "password updated failled")
+        }
 
         return res.status(200).json(
-           new ApiResponse( 200, updatedPassword,"Password updated Successfully")
+            new ApiResponse(200, updatedPassword, "Password updated Successfully")
         );
 
     } catch (error) {
@@ -167,14 +247,14 @@ const UpdateAvatar = Asynchandler(async (req, res) => {
     try {
         const avatar = req.file?.avatar[0]
         const userId = req.user?._id
-        if(!avatar){
+        if (!avatar) {
             throw new ApiError(404, "Please upload an Image");
         }
         const updatedAvatar = await User.findByIdAndUpdate(userId);
         return res.status(200).json(
             new ApiResponse(200, updatedAvatar, "Avatar updated Successfully")
         )
-        
+
     } catch (error) {
         res.status(500).json(
             new ApiError(500, error?.message)
@@ -187,22 +267,29 @@ const UpdateUserDetails = Asynchandler(async (req, res) => {
         const { fullName, email, phone } = req.body
         const userId = req.user?._id;
 
-        if([fullName, email, phone].some((data)=> data === " ")){
-            throw new ApiError(404, "Provide some data to update");
-        }
-        
-        if([fullName, email, phone].some((data)=> data===req.body)){
-            throw new ApiError(404, "Sorry you cannot update the data already exist");
+        if ([fullName, email, phone].some((data) => data === "")) {
+            throw new ApiError(404, "Input fields must not be empty");
         }
 
-       const updatedUser = await User.findByIdAndUpdate(userId,
-        {fullName,email,phone},
-        {new:true},
-       );
-       return res.status(200).json(
-       new ApiResponse(200, updatedUser, "Userdetails updated Successfully")
-       )
-      
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Unauthorized ! Invalid userId")
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            {
+                $set: {
+                    fullName,
+                    email,
+                    phone
+                }
+            },
+            { new: true },
+        );
+        return res.status(200).json(
+            new ApiResponse(200, updatedUser, "Userdetails updated Successfully")
+        )
+
     } catch (error) {
         res.status(500).json(
             new ApiError(500, error?.message)
@@ -212,7 +299,7 @@ const UpdateUserDetails = Asynchandler(async (req, res) => {
 
 export {
     Registeruser,
-    Loginuser,  
+    Loginuser,
     Logoutuser,
     GetCurrentuser,
     ChangePassword,
